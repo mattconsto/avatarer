928893cece15af0429f763b61d7be2ae40b18720
Matthew Consterdine <mrconsterdine@googlemail.com>
2019-02-13 01:46:39 +0000

    Refactor, use a virtual dom, make it looks nice, add more avatars

--- random.js
+++ random.js
@@ -1,23 +1,52 @@
-function xfnv1a(str) {
-	for(var i = 0, h = 2166136261 >>> 0; i < str.length; i++)
-		h = Math.imul(h ^ str.charCodeAt(i), 16777619);
-	return function() {
-		h += h << 13; h ^= h >>> 7;
-		h += h << 3;  h ^= h >>> 17;
-		return (h += h << 5) >>> 0;
+var srand = function(str) {
+	var xfnv1a = function(str) {
+		for(var i = 0, h = 2166136261 >>> 0; i < str.length; i++)
+			h = Math.imul(h ^ str.charCodeAt(i), 16777619);
+		return function() {
+			h += h << 13; h ^= h >>> 7;
+			h += h << 3;  h ^= h >>> 17;
+			return (h += h << 5) >>> 0;
+		}
 	}
+
+	// Create a xfnv1a state
+	var seed = xfnv1a(str + '');
+
+	var sfc32 = function(a, b, c, d) {
+		return function() {
+			a >>>= 0; b >>>= 0; c >>>= 0; d >>>= 0; 
+			var t = (a + b) | 0;
+			a = b ^ b >>> 9;
+			b = c + (c << 3) | 0;
+			c = (c << 21 | c >>> 11);
+			d = d + 1 | 0;
+			t = t + d | 0;
+			c = c + t | 0;
+			return (t >>> 0) / 4294967296;
+		}
+	}
+
+	// Use four 32-bit hashes to provide the seed for sfc32
+	var r = sfc32(seed(), seed(), seed(), seed());
+
+	function leftpad(n, w, z) {
+		z = z || ' ';
+		n = n + '';
+		return n.length >= w ? n : new Array(w-n.length+1).join(z) + n;
+	}
+
+	return {
+		color: function() {
+			return '#' + leftpad(((r() * 0xffffff) & 0xffffff).toString(16), 6, '0');
+		},
+		fl: r,
+		int: function(max) {return Math.floor(r() * (max + 1));},
+		input: function() {return str},
+		pc: function() {return Math.floor(r() * 101) + '%';},
+	};
 }
 
-function sfc32(a, b, c, d) {
-	return function() {
-		a >>>= 0; b >>>= 0; c >>>= 0; d >>>= 0; 
-		var t = (a + b) | 0;
-		a = b ^ b >>> 9;
-		b = c + (c << 3) | 0;
-		c = (c << 21 | c >>> 11);
-		d = d + 1 | 0;
-		t = t + d | 0;
-		c = c + t | 0;
-		return (t >>> 0) / 4294967296;
-	}
+var rand = function() {
+	// Yeah, I know...
+	return srand(Math.random());
 }
---
+++ script.js
@@ -0,0 +1,80 @@
+// THis code is *really* bad, but I am lazy
+var input  = document.getElementById('input');
+var output = document.getElementById('output');
+
+var $svg = function(name, values, children) {
+	var node = document.createElementNS('http://www.w3.org/2000/svg', name);
+	for(var key in values) node.setAttribute(key, values[key]);
+	for(var key in children) {
+		var child = Array.isArray(children[key]) ?
+			$svg.apply(null, children[key]) :
+			document.createTextNode(children[key]);
+		node.appendChild(child);
+	}
+	return node;
+}
+
+var avatarers = {
+	circle: function(r) {
+		var node = [];
+		var circles = r.int(4) + 3;
+		for(var i = 0; i < circles; i++) {
+			node.push(['circle', {
+				cx:r.int(128), cy:r.int(128), r:r.int(50), fill:r.color()
+			}]);
+		}
+		return node;
+	},
+	letter: function(r) {
+		return [
+			['circle', {cx:64, cy:64, r:64, fill:r.color()}],
+			['text', {
+				x:64, y:64, 'text-anchor': 'middle',
+				'alignment-baseline': 'central', 'font-size':100,
+			}, [(r.input() ? r.input() : '?').substr(0, 1)]]
+		];
+	},
+	gradient: function(r) {
+		return [
+			['defs', {}, [
+				['linearGradient', {id:'g',x1:r.pc(),y1:r.pc(),x2:r.pc(),y2:r.pc()}, [
+					['stop', {offset:'0%', 'stop-color':r.color()}],
+					['stop', {offset:'100%', 'stop-color':r.color()}]
+				]]
+			]],
+			['rect', {x:0, y:0, width:128, height:128, fill:'url(#g)'}]
+		];
+	},
+	profile: function(r) {
+		return [
+			['path', {d: 'M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm95.8 32.6L272 480l-32-136 32-56h-96l32 56-32 136-47.8-191.4C56.9 292 0 350.3 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-72.1-56.9-130.4-128.2-133.8z', transform: 'scale(0.2) translate(96, 64)', fill:r.color()}],
+		];
+	},
+	square: function(r) {
+		return [
+			['rect', {x:0, y:0, width:64, height:64, fill:r.color()}],
+			['rect', {x:64, y:0, width:64, height:64, fill:r.color()}],
+			['rect', {x:0, y:64, width:64, height:64, fill:r.color()}],
+			['rect', {x:64, y:64, width:64, height:64, fill:r.color()}],
+		];
+	},
+}
+
+var placerer = function () {
+	while(output.firstChild) output.removeChild(output.firstChild);
+	var i = 0;
+	for(var key in avatarers) {
+		i += 1;
+		var svg = $svg('svg',
+			{height:'256px', width:'256px', viewBox:'0 0 128 128'},
+			avatarers[key](srand(input.value))
+		);
+		output.appendChild(svg);
+		canvg(document.getElementById('canvas' + i), svg.outerHTML);
+	}
+}
+
+window.addEventListener('load', function() {
+	input.addEventListener('input', placerer);
+	input.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
+})
